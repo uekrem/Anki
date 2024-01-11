20cf86af0d4c8a0586a60606a46ceabeadd8c301
uekrem <utkuekrem89@gmail.com>
2024-01-11 14:10:31 +0300

    anki

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="new/register.css">
+    <link rel="stylesheet" href="new/index.css">
 </head>
 <body>
     <main>
